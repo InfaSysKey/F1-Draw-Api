a28f0b39ce31699679aa0c9e5324bdfac63da859
Diego Domínguez <diego.dominguez@Diversius.com>
2023-01-12 14:45:29 +0100

    Add springboot project

---
+++ f1-draw-api/src/main/kotlin/com/Infasyskey/f1drawapi/F1DrawApiApplication.kt
@@ -0,0 +1,11 @@
+package com.Infasyskey.f1drawapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class F1DrawApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<F1DrawApiApplication>(*args)
+}
---
+++ f1-draw-api/src/test/kotlin/com/Infasyskey/f1drawapi/F1DrawApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.Infasyskey.f1drawapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class F1DrawApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
